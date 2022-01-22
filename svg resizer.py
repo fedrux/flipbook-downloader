9ebc9bd58028fb3e64e00895bbcb3a5d3c81b8d6
Federico Adreani <fede1397@gmail.com>
2022-01-22 22:53:36 +0100

    Update svg resizer.py

--- svg resizer.py
+++ svg resizer.py
@@ -1,12 +1,34 @@
-replacements = {'594.453':'2972.265', '814.208':'4071.04'}
-for x in range(1, 689):
+
+from xml.dom import minidom
+
+nomeLibro=""
+numeroPagine=
+
+
+
+cartella = '/'+nomeLibro+'/'
+
+
+for x in range(1, numeroPagine+1):
+	print(str(x/numeroPagine*100) + "% --> " + str(x))
 	try: 
 		r= str(x).zfill(4)
+		
 
-		with open(r+".svg") as infile, open(r+"-resize.svg", 'w') as outfile:
+		with open(cartella+r+".svg") as infile, open(cartella+r+"-resize.svg", 'w') as outfile:
 			for line in infile:
+				
+				width = minidom.parseString(line).getElementsByTagName("svg:svg")[0].getAttribute("width")[:-2] # elimina px
+				height = minidom.parseString(line).getElementsByTagName("svg:svg")[0].getAttribute("height")[:-2]
+				
+				replacements = {width:str(float(width)*4), height:str(float(height)*4)}#fare in scala 4 a 1, deve cambiare di libro in libro
+				
+				
+				
 				for src, target in replacements.items():
-					line = line.replace(src, target, 1)
+					line = line.replace(src, target, 1)#lasciare sempre 1, non deve cambiare la viewbox
 				outfile.write(line)
+		
+		
 		
 	except Exception as e: print(e)
