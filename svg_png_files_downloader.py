6dac461accebe4e6b66d07c3422c4bb7118b59bf
Federico Adreani <fede1397@gmail.com>
2022-01-22 22:51:21 +0100

    Update svg_png_files_downloader.py

--- svg_png_files_downloader.py
+++ svg_png_files_downloader.py
@@ -1,5 +1,5 @@
 '''
-books are made of 2 FILE: svg e png
+i libri sono composti da 2 FILE: svg e png
 '''
 from seleniumwire import webdriver
 import time
@@ -10,33 +10,57 @@ import requests
 from PIL import Image
 from io import StringIO
 
+nomeLibro=""
+numeroPagine=
+
+
+
 options = webdriver.ChromeOptions() 
-options.add_argument("user-data-dir=/home/<user>/.config/google-chrome/Default")
-prefs = {'download.default_directory' : '/Downloads'}
-options.add_experimental_option('prefs', prefs)
+options.add_argument("user-data-dir=/home/_____usrdname______/.config/google-chrome/Default")
 
 br = webdriver.Chrome(chrome_options=options)
 
+
 #start it up
+
 print("Avvio di chrome....")
-br.get("url link to book")
+br.get("")
 
-##wait time to access the book if the access is behind registration
 
-print("Press Enter to start");
+print("Quando vuoi avviare la sequenza premi invio");
 input()
 
-for x in range(1, 689):
+import os
+if not os.path.exists('/'+nomeLibro+'/'):
+    os.makedirs('/'+nomeLibro+'/')
+
+
+for x in range(1, numeroPagine+1):
+	print(str(x/numeroPagine*100) + "% --> " + str(x))
 	try:
 		r= str(x).zfill(4)
-		 #pages ar form 0001.svg or .png to 0689.svg
 		
-		br.get("pngURL/'+r+'.png', 'wb') as file:
+		
+		urlImage = "/"+nomeLibro+"/"+str(r)+"_1.jpg?uni="  #change here
+		
+		br.get(urlImage)
+		with open('/'+nomeLibro+'/'+r+'.png', 'wb') as file:
 			file.write(br.find_element_by_tag_name('img').screenshot_as_png) #se non uso screenshot ma uso altro ottengo file migliore
 		
+		urlSvg="/"+nomeLibro+"/"+str(r)+".svg?uni="  #change here
+		
+		br.get(urlSvg)
+		with open('/'+nomeLibro+'/'+r+'.svg', 'wb') as file:
+			file.write(br.page_source.encode("utf-8")) #se non uso screenshot ma uso altro ottengo file migliore
+		
+	
 			
-		br.get("svgURL/"+str(r)+".svg")
-		br.find_element_by_tag_name('svg').screenshot('/svg-'+r+'.png', 'wb') as file:
-			file.write(br.find_element_by_tag_name('svg'))
-	except Exception as e: print(e)
+			
+			
+	except Exception as e: 
+		print(e)
+		print(urlImage)
+		print(urlSvg)
+
+
 print("Finish....")
