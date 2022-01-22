219fb5f2c6027cce504b8abd6bde80f529735ca0
Federico Adreani <fede1397@gmail.com>
2022-01-22 22:54:34 +0100

    Update generator.sh

--- generator.sh
+++ generator.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
-mkdir -p out
 
-for i in $(seq 1 689); do 
+cd "./"
+
+
+#non mettere spazi tra definizione variabile e uguale e valore
+
+
+nomeLibro=""
+numeroPagine=
+
+
+mkdir -p $nomeLibro/out
+
+
+for i in $(seq 1 $numeroPagine); do #inserire da 1 al numero di page
 
     page=`printf "%04d" $i`;
     
-	wkhtmltoimage --transparent  pages1/${page}-resize.svg pages1/py-svg/${page}.png
+	wkhtmltoimage --transparent  $nomeLibro/${page}-resize.svg $nomeLibro/SVG_${page}.png
 	
-    convert -flatten -geometry 2972x4071 -fuzz 10% -transparent white  -density 400   pages1/${page}.png pages1/py-svg/${page}.png out/$page.pdf
+    convert -flatten -geometry 2972x4071 -fuzz 10% -transparent white  -density 400   $nomeLibro/${page}.png $nomeLibro/SVG_${page}.png $nomeLibro/out/$page.pdf
     
+    rm $nomeLibro/SVG_${page}.png
+	
 	echo ${page} 
     
     
     
 done;
-cd out
-pdfunite * out.pdf
+cd $nomeLibro/out
+pdfunite * $nomeLibro.pdf
